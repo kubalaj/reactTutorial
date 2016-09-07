08b670dd4d1f16debdf14c6ac023ca76d6ed3fcc
Paul Kubala <paulisdaman@gmail.com>
2016-09-07 14:41:46 -0700

    adding more

---
+++ public/scripts/example.js
@@ -0,0 +1,33 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world I am a comment list.
+      </div>
+    );
+  }
+});
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world I am a Comment Form.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
